488e3fe8f4d6ba654e160ee1b2aff664e66b022a
Baconm145 <Baconm145@gmail.com>
2018-05-16 17:35:25 +0300

    added control.php

---
+++ control.php
@@ -0,0 +1,9 @@
+<?php
+	system ( gpio mode 1 out );
+	system ( gpio mode 4 out );
+	system ( gpio mode 5 out );
+	system ( gpio mode 7 out );
+	system ( gpio mode 0 out );
+	system ( gpio mode 3 out );
+	system ( gpio write 3 1 );
+?>
--- js/control.js
+++ js/control.js
@@ -1,7 +1,50 @@
-document.onkeydown = function( event ) {
-	console.log( event );
+function sendControlRequest( pkg ) {
+	var xhr = new XMLHttpRequest();
+    var params = 'name=' + encodeURIComponent( pkg );
+    xhr.open("GET", '/?' + params, true);
+    xhr.send();
 }
 
 document.onkeyup = function( event ) {
 	console.log( event.key );
+	//lfs = left side forward start
+	//rfs = right side forward start
+	//lbs = left side backward start
+	//rbs = right side backward start
+	if ( event.key == "w" ) {
+		sendControlRequest( "lfs" );
+		sendControlRequest( "rfs" );
+	}
+	if ( event.key == "s" ) {
+		sendControlRequest( "lbs" );
+		sendControlRequest( "rbs" );
+	}
+	if ( event.key == "a" ) {
+		sendControlRequest( "rfs" );
+	}
+	if ( event.key == "d" ) {
+		sendControlRequest( "lfs" );
+	}
+}
+
+document.onkeydown = function( event ) {
+	console.log( event );
+	//lfe = left side forward end
+	//rfe = right side forward end
+	//lbe = left side backward end
+	//rbe = right side backward end
+	if ( event.key == "w" ) {
+		sendControlRequest( "lfe" );
+		sendControlRequest( "rfe" );
+	}
+	if ( event.key == "s" ) {
+		sendControlRequest( "lbe" );
+		sendControlRequest( "rbe" );
+	}
+	if ( event.key == "a" ) {
+		sendControlRequest( "rfe" );
+	}
+	if ( event.key == "d" ) {
+		sendControlRequest( "lfe" );
+	}
 }
