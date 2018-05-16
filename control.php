0d7af35e9e0a9c2af09a3766d4c3f294f991d78f
Baconm145 <Baconm145@gmail.com>
2018-05-16 17:48:21 +0300

    hotfix

--- control.php
+++ control.php
@@ -1,12 +1,12 @@
 <?php
-	system( gpio mode 1 out );
-	system( gpio mode 4 out );
-	system( gpio mode 5 out );
-	system( gpio mode 7 out );
-	system( gpio mode 0 out );
-	system( gpio mode 3 out );
-	system( gpio write 7 1 );
-	system( gpio write 1 1 );
-	system( gpio write 3 1 );
-	system( gpio write 3 0 );
+	system( 'gpio mode 1 out' );
+	system( 'gpio mode 4 out' );
+	system( 'gpio mode 5 out' );
+	system( 'gpio mode 7 out' );
+	system( 'gpio mode 0 out' );
+	system( 'gpio mode 3 out' );
+	system( 'gpio write 7 1' );
+	system( 'gpio write 1 1' );
+	system( 'gpio write 3 1' );
+	system( 'gpio write 3 0' );
 ?>
