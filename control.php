719fb86fac468f0b0c0eceb0c0ab7e8f12e5c14d
Baconm145 <Baconm145@gmail.com>
2018-05-20 16:14:02 +0300

    hotfix

--- control.php
+++ control.php
@@ -10,19 +10,19 @@
 	}
 	
 	if ( $q == rfs ) {
-		system( 'echo "23='.$speed.'" > /dev/pi-blaster' );
+		system( 'echo "23=$speed" > /dev/pi-blaster' );
 		system( 'echo "24=0" > /dev/pi-blaster' );
 	}
 	if ( $q == lfs ) {
 		system( 'echo "17=0" > /dev/pi-blaster' );
-		system( 'echo "22='.$speed.'" > /dev/pi-blaster' );
+		system( 'echo "22=$speed" > /dev/pi-blaster' );
 	}
 	if ( $q == rbs ) {
 		system( 'echo "23=0" > /dev/pi-blaster' );
-		system( 'echo "24='.$speed.'" > /dev/pi-blaster' );
+		system( 'echo "24=$speed" > /dev/pi-blaster' );
 	}
 	if ( $q == lbs ) {
-		system( 'echo "17='.$speed.'" > /dev/pi-blaster' );
+		system( 'echo "17=$speed" > /dev/pi-blaster' );
 		system( 'echo "22=0" > /dev/pi-blaster' );
 	}
 	if ( $q == lfe ) {
