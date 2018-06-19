3c68dba6137213c4159d383071abae1fd1ea9bd4
Baconm145 <Baconm145@gmail.com>
2018-06-19 18:02:33 +0300

    added control page

--- index.php
+++ index.php
@@ -301,7 +301,7 @@
 	
 	div.foot left {
 		float: left;
-		width: 95%;
+		width: 87%;
 	}
 	div.foot right {
 		text-align: right;
@@ -661,6 +661,7 @@
 	<div class="foot">
 		<left>&copy; Vladimir Busyrev, Kulaev Ivan</left>
 		<a href="http://www.google.ru">Разработчики</a>
+		<a href="http://www.google.ru">Принцип работы</a>
 	</div>
       <?php if ($debugString != "") echo "$debugString<br>"; ?>
    </body>
